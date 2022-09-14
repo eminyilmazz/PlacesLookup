3a47819c5a01e40665e0f218297133ce66b8ef62
Emin <eminyilmzz@gmail.com>
2022-09-15 00:56:27 +0300

    Initial commit

---
+++ src/test/java/com/emin/placeslookup/PlacesLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emin.placeslookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesLookupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
